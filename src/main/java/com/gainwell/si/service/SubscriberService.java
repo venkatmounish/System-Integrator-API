3f11f5971497ef624d8d76fdd744c1d1f12adadd
venkatmounish <venkatmounishg@gmail.com>
2021-12-07 01:01:39 -0600

    Update SubscriberService.java

--- src/main/java/com/gainwell/si/service/SubscriberService.java
+++ src/main/java/com/gainwell/si/service/SubscriberService.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import javax.rmi.CORBA.Util;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
